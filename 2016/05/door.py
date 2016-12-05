e1058cd1549d4bc0f6a4b34475b8117e7acd2d14
Thomas Marsh <tmarsh@spotify.com>
2016-12-05 00:32:36 -0500

    day 5

---
+++ 2016/05/door.py
@@ -0,0 +1,37 @@
+import md5
+
+test = 'abc'
+data = 'cxdnnyjw'
+
+prefix = data
+
+def part1(prefix):
+    i = 0
+    p = ''
+    for n in range(8):
+        while True:
+            x = prefix + str(i)
+            i += 1
+            digest = md5.md5(x).hexdigest()
+            if digest.startswith('00000'):
+                p += digest[5]
+                break
+    return p
+
+def part2(prefix):
+    i = 0
+    p = ['_' for _ in range(8)]
+    while p.count('_') != 0:
+        x = prefix + str(i)
+        i += 1
+        digest = md5.md5(x).hexdigest()
+        if digest.startswith('00000'):
+            pos = digest[5]
+            value = digest[6]
+            if pos.isdigit() and int(pos) < len(p) and p[int(pos)] == '_':
+                p[int(pos)] = value
+                print ''.join(p)
+    return ''.join(p)
+
+#part1(prefix)
+part2(prefix)
