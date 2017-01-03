2e218eb62d6c9a930aee07f0070e41c175c8d298
Thomas Marsh <tmarsh@spotify.com>
2017-01-03 13:19:01 -0500

    Cleanup

--- 2015/15/cookie.py
+++ 2015/15/cookie.py
@@ -16,48 +16,36 @@ test = [
 assert(score(test, [44, 56])[0] == 62842880)
 assert(score(test, [40, 60])[1] == 500)
 
-MAX = 100
-
-def d(n, m=0, l=[], s=0):
+def d(n, high=100, m=0, l=[], s=0):
     assert(n >= 1)
     if n == 1:
-        assert(sum(l) == s)
-        x = l[:] + [MAX-sum(l)]
-        assert(sum(x) == MAX)
-        return [[MAX-sum(l)]]
+        x = l[:] + [high-sum(l)]
+        return [[high-sum(l)]]
     else:
         w = []
-        for i in range(0, MAX+1-sum(l)):
+        for i in range(0, high+1-sum(l)):
             x = l[:] + [i]
-            for r in d(n-1, m+1, x, i+s):
+            for r in d(n-1, high, m+1, x, i+s):
                 w.append([i]+r)
         return w
 
-def find_numbers(s):
-    return re.findall(r'([+-]?\d+)', s)
-
 def load():
-    with open(sys.argv[1], 'r') as f:
+    with open(sys.argv[1] if len(sys.argv) > 1 else 'input', 'r') as f:
         lines = f.read().splitlines()
-        return [[int(n) for n in find_numbers(line)] for line in lines]
+        return [[int(n) for n in re.findall(r'[-]?\d+', line)] for line in lines]
 
 def search():
     data = load()
-    best = 0, None
-    for entry in d(len(data)):
-        s = score(data, entry)[0]
-        if s > best[0]:
-            best = s, entry
-    return best
-
-def search500():
-    data = load()
-    best = 0, None
+    best = 0
+    cal_best = 0
     for entry in d(len(data)):
         s, cal = score(data, entry)
-        if cal == 500 and s > best[0]:
-            best = s, entry
-    return best
+        if s > best:
+            best = s
+        if cal == 500 and s > cal_best:
+            cal_best = s
+    return best, cal_best
 
-print 'Part 1:', search()[0]
-print 'Part 2:', search500()[0]
+part1, part2 = search()
+print 'Part 1:', part1
+print 'Part 2:', part2
