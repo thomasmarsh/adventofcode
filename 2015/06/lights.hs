82942c1b9c142942362b600e98900c057517ce42
Thomas Marsh <tmarsh@spotify.com>
2017-03-20 22:53:32 -0400

    Use mutable array (still slow..)

--- 2015/06/lights.hs
+++ 2015/06/lights.hs
@@ -1,12 +1,14 @@
+{-# LANGUAGE FlexibleContexts #-}
+
 import System.Environment (getArgs)
-import Data.Array ((//), (!), array, Array, elems)
 import Data.List.Split (splitOn)
+import Data.Array.IO (IOUArray, MArray, newArray, readArray, writeArray)
 
 bound = 999
 
 type Coord = (Int, Int)
-type Lights = Array Coord Int
-data Mode = OnOff | Dimmable
+type Lights a = IOUArray Coord a
+data Mode = English | Elvish
 
 parseCoord :: String -> Coord
 parseCoord s = tuple $ map (read::String->Int) (splitOn "," s)
@@ -22,42 +24,40 @@ parseLine s = p ws
           p _ = error "parse error"
           tuple fn c1 c2 = (fn, parseCoord c1, parseCoord c2)
 
-initial :: Lights
-initial = array ((0, 0), (bound, bound))
-                [((x, y), 0) | x <- [0..bound],
-                               y <- [0..bound]]
-
 toggle :: Mode -> Int -> Int
-toggle OnOff n = 1-n
-toggle Dimmable n = n+2
+toggle English n = 1-n
+toggle Elvish n = n+2
 
 on :: Mode -> Int -> Int
-on OnOff n = 1
-on Dimmable n = n+1
+on English n = 1
+on Elvish n = n+1
 
 off :: Mode -> Int -> Int
-off OnOff n = 0 
-off Dimmable n
+off English n = 0 
+off Elvish n
     | n > 0 = n-1
     | otherwise = 0
 
-update :: Lights -> (Int -> Int) -> Coord -> Coord -> Lights
-update a fn (x1, y1) (x2, y2) = a // updates
-    where coords = [(x,y) | x <- [x1..x2], y <- [y1..y2]]
-          delta c = (c, fn (a!c))
-          updates = map delta coords
+update :: MArray IOUArray Int IO =>
+          Lights Int -> Mode -> ((Mode -> Int -> Int), Coord, Coord) -> IO ()
+update a m (fn, (x1, y1), (x2, y2)) = do
+    mapM_ update' [(x,y) | x <- [x1..x2], y <- [y1..y2]]
+    where update' c = do
+            b <- readArray a c
+            writeArray a c (fn m b)
+            return ()
 
-count :: Lights -> Int
-count a = sum $ elems a
-
-apply :: Mode -> Lights -> [(Mode -> Int -> Int, Coord, Coord)] -> Lights
-apply m a [] = a
-apply m a ((fn, c1, c2):xs) = apply m (update a (fn m) c1 c2) xs
+apply :: MArray IOUArray Int IO =>
+         Int -> Mode -> [(Mode -> Int -> Int, Coord, Coord)] -> IO ()
+apply part m xs = do
+    lights <- newArray ((0, 0), (bound, bound)) 0
+    mapM_ (update lights m) xs
+    ts <- mapM (readArray lights) [(x, y) | x <- [0..bound], y <- [0..bound]]
+    putStrLn $ "Part " ++ (show part) ++ ": " ++ show (sum ts)
 
 main = do
     [path] <- getArgs
     contents <- readFile path
     let commands = map parseLine (lines contents)
-    let lights = initial
-    putStrLn $ "Part 1: " ++ show (count (apply OnOff lights commands))
-    putStrLn $ "Part 2: " ++ show (count (apply Dimmable lights commands))
+    apply 1 English commands
+    apply 2 Elvish commands
