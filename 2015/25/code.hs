0a5e2960c759aba5e19a168abeb60b0801a4e042
Thomas Marsh <tmarsh@spotify.com>
2017-03-21 17:05:27 -0400

    2015 day 25 haskell

---
+++ 2015/25/code.hs
@@ -0,0 +1,13 @@
+target :: (Int, Int)
+target = (3029, 2947)
+
+search :: Int -> Int -> Int -> Int
+search x y z
+    | (x, y) == target = z
+    | y == 1 = search 1 (x+1) z'
+    | otherwise = search (x+1) (y-1) z'
+    where z' = (z * 252533) `mod` 33554393
+
+main :: IO ()
+main = do
+    print $ search 1 1 20151125
