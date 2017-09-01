c6a6a573a2f593d7553cdc937835049c0538bfb7
Thomas Marsh <tmarsh@spotify.com>
2017-09-01 12:55:37 -0400

    Precompile for faster execution

--- 2016/12/cpu.hs
+++ 2016/12/cpu.hs
@@ -4,6 +4,8 @@ import Data.Maybe (fromMaybe)
 import System.Environment (getArgs)
 import Text.Read (readMaybe)
 
+-- Types
+
 data State = State
     { rA :: Int
     , rB :: Int
@@ -12,70 +14,101 @@ data State = State
     , pc :: Int
     } deriving (Show)
 
-isRegister :: String -> Bool
-isRegister s = s `isInfixOf` "abcd"
+data Register
+    = A | B | C | D
+    deriving (Eq, Show)
 
-setReg :: State -> String -> Int -> State
-setReg state r n = case r of
-                    "a" -> state { rA = n }
-                    "b" -> state { rB = n }
-                    "c" -> state { rC = n }
-                    "d" -> state { rD = n }
+data Descriptor
+    = Reg Register
+    | Val Int
+    deriving (Eq, Show)
 
-getReg :: State -> String -> Int
-getReg state r = case r of
-                    "a" -> rA state
-                    "b" -> rB state
-                    "c" -> rC state
-                    "d" -> rD state
+data Instruction
+    = Cpy Descriptor Register
+    | Inc Register
+    | Dec Register
+    | Jnz Descriptor Int
+    deriving (Eq, Show)
 
-incPc :: State -> State
-incPc state = state { pc = pc state + 1 }
+type Program = [Instruction]
+
+-- Parsing
 
 readInt :: String -> Int
 readInt s = fromMaybe 0 (readMaybe s :: Maybe Int)
 
-cpy :: State -> [String] -> State
-cpy state [x, y]
-    | isRegister x = go (getReg state)
-    | otherwise = go readInt
-    where go f = incPc $ setReg state y (f x)
+parseReg :: String -> Register
+parseReg s
+    | isRegister s =
+        case s of
+            "a" -> A
+            "b" -> B
+            "c" -> C
+            "d" -> D
+    | otherwise = error "unknown register"
 
-inc :: State -> [String] -> State
-inc state [r] = incPc $ setReg state r (getReg state r + 1)
+isRegister :: String -> Bool
+isRegister s = s `isInfixOf` "abcd"
 
-dec :: State -> [String] -> State
-dec state [r] = incPc $ setReg state r (getReg state r - 1)
+parseLine :: [String] -> Instruction
+parseLine ["cpy", x, y]
+    | isRegister x = Cpy (Reg (parseReg x)) (parseReg y)
+    | otherwise = Cpy (Val (readInt x)) (parseReg y)
+parseLine ["inc", x] = Inc (parseReg x)
+parseLine ["dec", x] = Dec (parseReg x)
+parseLine ["jnz", x, y]
+    | isRegister x = Jnz (Reg (parseReg x)) (readInt y)
+    | otherwise = Jnz (Val (readInt x)) (readInt y)
 
-jnz :: State -> [String] -> State
-jnz state [x, y]
-    | q /= 0 = state { pc = pc state + readInt y }
-    | otherwise = incPc state
-    where q = if isRegister x
-              then getReg state x
-              else readInt y
+parse :: String -> Program
+parse s = map (parseLine . splitOn " ") (lines s)
 
-exec :: State -> [String] -> State
-exec state (x:xs) =
-    case x of 
-        "cpy" -> cpy state xs
-        "inc" -> inc state xs
-        "dec" -> dec state xs
-        "jnz" -> jnz state xs
+-- Execution
+
+setReg :: State -> Register -> Int -> State
+setReg st A n = st { rA = n }
+setReg st B n = st { rB = n }
+setReg st C n = st { rC = n }
+setReg st D n = st { rD = n }
+
+getReg :: State -> Register -> Int
+getReg st A = rA st
+getReg st B = rB st
+getReg st C = rC st
+getReg st D = rD st
+
+incPc :: State -> State
+incPc st = st { pc = pc st + 1 }
+
+doCpy :: State -> Register -> Int -> State
+doCpy st r n = incPc $ setReg st r n
+
+doJnz :: State -> Int -> Int -> State
+doJnz st q y
+    | q /= 0 = st { pc = pc st + y }
+    | otherwise = incPc st
+
+exec :: State -> Instruction -> State
+exec st (Cpy (Reg x) y) = doCpy st y (getReg st x)
+exec st (Cpy (Val x) y) = doCpy st y x
+exec st (Inc r)         = incPc $ setReg st r (getReg st r + 1)
+exec st (Dec r)         = incPc $ setReg st r (getReg st r - 1)
+exec st (Jnz (Reg x) n) = doJnz st (getReg st x) n
+exec st (Jnz (Val x) n) = doJnz st x n
 
 initial :: State
 initial = State { rA = 0, rB = 0, rC = 0, rD = 0, pc = 0 }
 
-run :: [[String]] -> State -> Int
+run :: Program -> State -> Int
 run prog init = rA $ go init
-    where go state
-            | pc state < 0 || pc state > (length prog -1) = state
-            | otherwise = go $ exec state (prog !! pc state)
+    where go st
+            | pc st < 0 || pc st > (length prog -1) = st
+            | otherwise = go $ exec st (prog !! pc st)
 
 main :: IO ()
 main = do
     [path] <- getArgs
     contents <- readFile path
-    let prog = map (splitOn " ") (lines contents)
+    let prog = parse contents
     putStrLn $ "Part 1: " ++ show (run prog initial)
     putStrLn $ "Part 2: " ++ show (run prog (initial { rC = 1 }))
