46a781fc8c026ebde6c0345d481acec5fc138577
Thomas Marsh <tmarsh@spotify.com>
2018-12-02 14:22:51 -0500

    Fix compile error

--- src/Y2017/D12.hs
+++ src/Y2017/D12.hs
@@ -1,25 +1,27 @@
--- {-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE ViewPatterns #-}
 
 module Y2017.D12 where
 
-{-  WIP
 import Data.Char (isDigit)
+import Data.List (unfoldr)
 import Data.Maybe (fromMaybe)
 import qualified Data.Map.Strict as M
+import Data.Map.Strict ((!?))
 import qualified Data.Set as S
 import Data.List.Split (splitOn)
 import System.Environment (getArgs)
 
 parse :: String -> M.Map Int [Int]
-parse = M.fromList . map parseLine . lines where
-    parseLine (words -> x : "<->" : xs) = (read x, read . filter isDigit <$> xs)
+parse = M.fromList . map parseLine . lines
+    where
+        parseLine (words -> x : "<->" : xs) = (read x, read . filter isDigit <$> xs)
 
 connected :: (Ord a) => M.Map a [a] -> a -> S.Set a
 connected neighbors = grow S.empty . (:[]) where
     grow s [] = s
     grow s (x:xs) = grow (S.insert x s) $ queue ++ xs where
-        queue = if S.member x s then [] 
-                else fromMaybe [] $ neighbors M.(!?) x
+        queue = if S.member x s then []
+                else fromMaybe [] $ neighbors !? x
 
 day12a :: String -> Int
 day12a = S.size . flip connected 0 . parse
@@ -35,4 +37,3 @@ main = do
     [path] <- getArgs
     contents <- readFile path
     print $ parse contents
--}
