8717a90de2a4d1efed383ea626b55a0ce99b41f3
Thomas Marsh <tmarsh@spotify.com>
2018-12-02 14:22:04 -0500

    Add 2018 day 1

---
+++ src/Y2018/D01.hs
@@ -0,0 +1,26 @@
+module Y2018.D01 where
+
+import           Data.IntSet (IntSet)
+import qualified Data.IntSet as S
+
+parseNum :: String -> Int
+parseNum x =
+    case head x of
+        '+' -> read $ tail x
+        _ -> read x
+
+repeatedFreq :: [Int] -> Int
+repeatedFreq xs = findDup S.empty xs'
+    where
+        xs' = scanl1 (+) (cycle xs)
+        findDup :: IntSet -> [Int] -> Int
+        findDup z (y:ys)
+            | S.member y z = y
+            | otherwise    = findDup (S.insert y z) ys
+
+run :: String -> IO ()
+run path = do
+    contents <- readFile path
+    let ls = map parseNum $ lines contents
+    putStrLn $ "Part 1: " ++ (show $ sum ls)
+    putStrLn $ "Part 2: " ++ (show $ repeatedFreq ls)
