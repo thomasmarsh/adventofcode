8e2830b01a04b39c6e77f1efe1bc487573dc4b33
Thomas Marsh <tmarsh@spotify.com>
2018-12-02 20:07:05 -0500

    Add 2018 day 2

---
+++ src/Y2018/D02.hs
@@ -0,0 +1,33 @@
+module Y2018.D02 where
+
+import Data.List (sort, group, tails)
+import Data.Maybe (catMaybes)
+
+checksum :: [String] -> Int
+checksum xs = count 2 * count 3
+    where
+        lengths = map length . group . sort
+        ys = map lengths xs
+        count n = length $ filter (elem n) ys
+
+matchPair :: (String, String) -> Maybe String
+matchPair (a, b)
+    | length a /= length b     = error "length mismatch"
+    | length s == length a - 1 = Just s
+    | otherwise                = Nothing
+    where
+        s = map fst $ filter (\(x, y) -> x == y) (zip a b)
+
+findClosest :: [String] -> String
+findClosest xs
+    -- n.b.: Safe.headMay
+    = head $ catMaybes $ map matchPair pairs
+    where
+        pairs = [(x,y) | (x:ys) <- tails xs, y <- ys]
+
+run :: String -> IO ()
+run path = do
+    contents <- readFile path
+    let xs = lines contents
+    putStrLn $ "Part 1: " ++ (show $ checksum xs)
+    putStrLn $ "Part 2: " ++ findClosest xs
